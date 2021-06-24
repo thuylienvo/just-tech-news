bee10c9cde028fc6f98434708f6a3d1544bcda1e
Mimi Vo <mimzy414@gmail.com>
2021-06-23 22:53:44 -0500

    Create a login and signup page

--- server.js
+++ server.js
@@ -24,3 +24,19 @@ const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
